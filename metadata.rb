21250a8d9057a586c746fbd6a6bd4c4d9286c7e2
em <eugen.mayer@kontextwork.de>
2017-02-13 16:56:23 +0100

    fix source_url / issues_url

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eugen.mayer@kontextwork.de'
 license 'Apache 2.0'
 description 'Configures and install Shorewall 4 and 5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 recipe 'shorewall_ng', 'Configures and install Shorewall 4 and 5'
 
@@ -13,3 +13,6 @@ recipe 'shorewall_ng', 'Configures and install Shorewall 4 and 5'
 end
 
 depends 'apt'
+
+source_url 'https://github.com/EugenMayer/chef-shorewall_ng'
+issues_url 'https://github.com/EugenMayer/chef-shorewall_ng/issues'
