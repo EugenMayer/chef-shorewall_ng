5a6ea11a06b07ce5f77789ef8fe1228af6555846
em <eugen.mayer@kontextwork.de>
2021-02-17 17:21:20 +0100

    ensure we have support for predictable interface names:

--- test/cookbooks/shorewall_ng_test/attributes/default.rb
+++ test/cookbooks/shorewall_ng_test/attributes/default.rb
@@ -1,26 +1,26 @@
 default[:shorewall][:enabled] = true
 
 default[:shorewall][:interfaces] = [
-    {'zone' => 'net', 'interface' => 'eth0', 'broadcast'=> 'detect', 'options' =>'dhcp,tcpflags,nosmurfs'}
+  { 'zone' => 'net', 'interface' => node['network']['default_interface'], 'broadcast' => 'detect', 'options' => 'dhcp,tcpflags,nosmurfs' }
 ]
 
 default[:shorewall][:zones] = [
-    {'zone' => 'fw', 'type' => 'firewall'},
-    {'zone' => 'net', 'type' => 'ipv4'}
+  { 'zone' => 'fw', 'type' => 'firewall' },
+  { 'zone' => 'net', 'type' => 'ipv4' }
 ]
 
 default[:shorewall][:policy] = [
-    {'source' => 'fw', 'dest' => 'all','policy' => :ACCEPT },
-    {'source' => 'all', 'dest' => 'all', 'policy' => :REJECT }
+  { 'source' => 'fw', 'dest' => 'all', 'policy' => :ACCEPT },
+  { 'source' => 'all', 'dest' => 'all', 'policy' => :REJECT }
 ]
 
 node.default[:shorewall][:rules].push({
-    'description' => 'Incoming SSH to firewall',
-    'source' => 'all',
-    'dest' => :fw,
-    'proto' => :tcp,
-    'dest_port' => 22,
-    'action' => :ACCEPT
-})
+                                        'description' => 'Incoming SSH to firewall',
+                                        'source' => 'all',
+                                        'dest' => :fw,
+                                        'proto' => :tcp,
+                                        'dest_port' => 22,
+                                        'action' => :ACCEPT
+                                      })
 
-node.default[:shorewall][:config][:ip_forwarding]='Yes'
+node.default[:shorewall][:config][:ip_forwarding] = 'Yes'
